9785b16bb0ef3b4040558c3dff78116f1f04bf8a
Qiusheng Wu <giswqs@gmail.com>
2022-12-22 12:50:43 -0500

    Changed cron schedule to 30 mins

--- src/bot.js
+++ src/bot.js
@@ -18,10 +18,9 @@ const retweet = () => {
       lang: "en",
     };
     TwitterBot.get("search/tweets", params, (err, data) => {
-      console.log(`====> Retweeting ${tags[i]}`);
       // when no errors
       if (!err) {
-        console.log(`====> Number of Tweets: ${data.statuses.length}`);
+        console.log(`====> Number of ${tags[i]} Tweets: ${data.statuses.length}`);
         for (let i = 0; i < data.statuses.length; i++) {
           let retweetID = data.statuses[i].id_str;
           TwitterBot.post(
@@ -46,102 +45,3 @@ const retweet = () => {
 }}
 
 retweet();
-
-
-
-
-  // console.log("====> Retweeting #EarthEngine");
-  // const params = {
-  //   q: "#EarthEngine",
-  //   result_type: "recent",
-  //   count: 20,
-  //   lang: "en",
-  // };
-  // TwitterBot.get("search/tweets", params, (err, data) => {
-  //   // when no errors
-  //   if (!err) {
-  //     console.log(`====> Number of Tweets: ${data.statuses.length}`);
-  //     for (let i = 0; i < data.statuses.length; i++) {
-  //       let retweetID = data.statuses[i].id_str;
-  //       TwitterBot.post(
-  //         "statuses/retweet/:id",
-  //         { id: retweetID },
-  //         (err, res) => {
-  //           if (res) {
-  //             console.log(`====> RETWEET SUCCESS ${retweetID}`);
-  //           }
-  //           if (err) {
-  //             console.log(`====> ERROR in RETWEET ${err}`);
-  //           }
-  //         }
-  //       );        
-  //     }        
-
-  //   } else {
-  //     console.log(`====> ERROR ${err}`);
-  //   }
-  // });
-
-  // console.log("====> Retweeting #GoogleEarthEngine");
-  // const params2 = {
-  //   q: "#GoogleEarthEngine",
-  //   result_type: "recent",
-  //   count: 20,
-  //   lang: "en",
-  // };
-  // TwitterBot.get("search/tweets", params2, (err, data) => {
-  //   // when no errors
-  //   if (!err) {
-  //     console.log(`====> Number of Tweets: ${data.statuses.length}`);
-  //     for (let i = 0; i < data.statuses.length; i++) {
-  //       let retweetID = data.statuses[i].id_str;
-  //       TwitterBot.post(
-  //         "statuses/retweet/:id",
-  //         { id: retweetID },
-  //         (err, res) => {
-  //           if (res) {
-  //             console.log(`====> RETWEET SUCCESS ${retweetID}`);
-  //           }
-  //           if (err) {
-  //             console.log(`====> ERROR in RETWEET ${err}`);
-  //           }
-  //         }
-  //       );        
-  //     }        
-
-  //   } else {
-  //     console.log(`====> ERROR ${err}`);
-  //   }
-  // });
-
-  // console.log("====> Retweeting #geemap");
-  // const params3 = {
-  //   q: "#geemap",
-  //   result_type: "recent",
-  //   count: 10,
-  //   lang: "en",
-  // };
-  // TwitterBot.get("search/tweets", params3, (err, data) => {
-  //   // when no errors
-  //   if (!err) {
-  //     console.log(`====> Number of Tweets: ${data.statuses.length}`);
-  //     for (let i = 0; i < data.statuses.length; i++) {
-  //       let retweetID = data.statuses[i].id_str;
-  //       TwitterBot.post(
-  //         "statuses/retweet/:id",
-  //         { id: retweetID },
-  //         (err, res) => {
-  //           if (res) {
-  //             console.log(`====> RETWEET SUCCESS ${retweetID}`);
-  //           }
-  //           if (err) {
-  //             console.log(`====> ERROR in RETWEET ${err}`);
-  //           }
-  //         }
-  //       );        
-  //     }        
-
-  //   } else {
-  //     console.log(`====> ERROR ${err}`);
-  //   }
-  // });
