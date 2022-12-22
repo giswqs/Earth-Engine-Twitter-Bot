eb73243ed3e7bf431ca7f09b2b00cfdc87c06f0e
Qiusheng Wu <giswqs@gmail.com>
2022-12-22 12:18:55 -0500

    Added #geemap tag

--- src/bot.js
+++ src/bot.js
@@ -41,10 +41,42 @@ const retweet = () => {
   const params2 = {
     q: "#GoogleEarthEngine",
     result_type: "recent",
-    count: 50,
+    count: 20,
     lang: "en",
   };
   TwitterBot.get("search/tweets", params2, (err, data) => {
+    // when no errors
+    if (!err) {
+      console.log(`====> Number of Tweets: ${data.statuses.length}`);
+      for (let i = 0; i < data.statuses.length; i++) {
+        let retweetID = data.statuses[i].id_str;
+        TwitterBot.post(
+          "statuses/retweet/:id",
+          { id: retweetID },
+          (err, res) => {
+            if (res) {
+              console.log(`====> RETWEET SUCCESS ${retweetID}`);
+            }
+            if (err) {
+              console.log(`====> ERROR in RETWEET ${err}`);
+            }
+          }
+        );        
+      }        
+
+    } else {
+      console.log(`====> ERROR ${err}`);
+    }
+  });
+
+  console.log("====> Retweeting #geemap");
+  const params3 = {
+    q: "#geemap",
+    result_type: "recent",
+    count: 10,
+    lang: "en",
+  };
+  TwitterBot.get("search/tweets", params3, (err, data) => {
     // when no errors
     if (!err) {
       console.log(`====> Number of Tweets: ${data.statuses.length}`);
